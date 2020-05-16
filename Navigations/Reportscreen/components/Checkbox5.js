3e4c58f5d370112f8f8ea38b832864338b913597
andhikagema09 <60083891+andhikagema09@users.noreply.github.com>
2020-05-16 08:54:10 +0700

    Add files via upload

---
+++ Navigations/Reportscreen/components/Checkbox5.js
@@ -0,0 +1,36 @@
+import React, {memo, useState, useCallback} from 'react';
+import CheckBox from '@react-native-community/checkbox';
+import {View, StyleSheet, Text} from 'react-native';
+
+const Checkbox5 = ({isChecked, handleOnChange}) => {
+  return (
+    <View style={styles.container}>
+      <CheckBox
+        tintColors={{
+          true: '#B32727',
+        }}
+        onValueChange={handleOnChange}
+        value={isChecked}
+      />
+      <Text style={styles.textStyle}>Angkot berhenti di sembarang tempat</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  textStyle: {
+    width: 140,
+    height: 18,
+    fontSize: 13,
+    lineHeight: 17,
+    color: '#B32727',
+  },
+});
+
+export default memo(Checkbox5);
---
+++ Navigations/Reportscreen/components/Checkbox6.js
@@ -0,0 +1,36 @@
+import React, {memo, useState, useCallback} from 'react';
+import CheckBox from '@react-native-community/checkbox';
+import {View, StyleSheet, Text} from 'react-native';
+
+const Checkbox6 = ({isChecked, handleOnChange}) => {
+  return (
+    <View style={styles.container}>
+      <CheckBox
+        tintColors={{
+          true: '#B32727',
+        }}
+        onValueChange={handleOnChange}
+        value={isChecked}
+      />
+      <Text style={styles.textStyle}>Lainnya</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  textStyle: {
+    width: 140,
+    height: 18,
+    fontSize: 13,
+    lineHeight: 17,
+    color: '#B32727',
+  },
+});
+
+export default memo(Checkbox6);
