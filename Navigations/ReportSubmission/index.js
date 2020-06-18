8882ac17ec0f68115f3fe4429c12b3266d3e6818
andhikagema09 <60083891+andhikagema09@users.noreply.github.com>
2020-06-18 20:54:13 +0700

    Update index.js

--- Navigations/ReportSubmission/index.js
+++ Navigations/ReportSubmission/index.js
@@ -2,9 +2,11 @@ import React, {memo, useContext, useEffect} from 'react';
 import {Text} from 'react-native';
 import {GlobalContext, GlobalContextDispatch} from '../../context';
 import {resetQrData} from '../../context/actions';
+import {useNavigation} from '@react-navigation/native';
 
 const ReportSubmission = () => {
   const {qrUseCase} = useContext(GlobalContext);
+  const navigation = useNavigation();
   const dispatch = useContext(GlobalContextDispatch);
   /**
    * reset qr data if screen close
@@ -12,8 +14,9 @@ const ReportSubmission = () => {
   useEffect(() => {
     return () => {
       dispatch({type: resetQrData});
+      navigation.navigate('Report Screen');
     };
-  }, [dispatch]);
+  }, [dispatch, navigation]);
   return <Text>{JSON.stringify(qrUseCase)}</Text>;
 };
 
